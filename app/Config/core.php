8b68bd82115d76ae2105202c246f7ae9a8cdb723
Kostya Shkryob <shkryob@ya.ru>
2014-12-14 02:57:25 +0200

    Setup debug kit and salts

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'VDoi3294838foASOVVEoegie2348923dvnOviVIOiu2388');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '9124127678569571295761295761295197');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
