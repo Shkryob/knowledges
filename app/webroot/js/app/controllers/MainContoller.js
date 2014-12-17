ee0ddbb3c860fa3a55680850ce82ac2cd6b69715
Kostya Shkryob <shkryob@ya.ru>
2014-12-17 12:45:22 +0200

    Login, logout and register

--- app/webroot/js/app/controllers/MainContoller.js
+++ app/webroot/js/app/controllers/MainContoller.js
@@ -1,29 +1,29 @@
-app.controller('MainController', ['$scope', '$location', 'Users', function ($scope, $location, Users) {
-    Users.current(function (data) {
-        $scope.currentUser = data;
-        if ($scope.isLoggedIn()) {
-            $location.path('/users/');
-        } else {
-            $location.path('/login/');
-        }
-    });
-      
-    $scope.showError = function (message) {
-        alert(message);
-    };
-    
-    $scope.setUser = function (user) {
-        $scope.currentUser = user;
-    };
-    
-    $scope.logout = function (event) {
-        event.preventDefault();
-        Users.logout();
-        $scope.currentUser = {};
-        $location.path('/login/');
-    };
-    
-    $scope.isLoggedIn = function() {
-        return $scope.currentUser && $scope.currentUser.email;
-    }
+pp.controller('MainController', ['$scope', '$location', 'Users', function ($scope, $location, Users) {
+   Users.current(function (data) {
+       $scope.currentUser = data;
+       if ($scope.isLoggedIn()) {
+           $location.path('/users/');
+       } else {
+           $location.path('/login/');
+       }
+   });
+     
+   $scope.showError = function (message) {
+       alert(message);
+   };
+   
+   $scope.setUser = function (user) {
+       $scope.currentUser = user;
+   };
+   
+   $scope.logout = function (event) {
+       event.preventDefault();
+       Users.logout();
+       $scope.currentUser = {};
+       $location.path('/login/');
+   };
+   
+   $scope.isLoggedIn = function() {
+       return $scope.currentUser && $scope.currentUser.email;
+   };
 }]);
