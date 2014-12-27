024475f0012c16b76ede49ff980c531781fec101
Kostya Shkryob <shkryob@ya.ru>
2014-12-27 22:11:33 +0200

    Show lessons by rows

--- app/webroot/js/app/controllers/answers/ListController.js
+++ app/webroot/js/app/controllers/answers/ListController.js
@@ -1,8 +1,20 @@
 app.controller('AnswersListController', ['$scope', 'Lessons', function ($scope, Lessons) {
-    $scope.lessons = [];
+    $scope.rows = [];
 
     $scope.getLessons = function () {
-        $scope.lessons = Lessons.query();
+        var lessons = Lessons.query(function (){
+            var row = [];
+            angular.forEach(lessons, function (val, key) {
+                if (row.length === 4) {
+                    $scope.rows.push(row);
+                    row = [];
+                }
+                row.push(val);
+            });
+            if (row.length > 0) {
+                $scope.rows.push(row);
+            }
+        });
     };
     
     $scope.delete = function (id) {
