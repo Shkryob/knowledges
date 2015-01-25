4d305b46712327051c1e42732800404a2982f112
Kostya Shkryob <shkryob@ya.ru>
2015-01-25 20:42:30 +0200

    Add test db

--- app/Config/database.php
+++ app/Config/database.php
@@ -13,18 +13,16 @@ class DATABASE_CONFIG {
 //		//'encoding' => 'utf8',
 //	);
 //
-//	public $test = array(
-//		'datasource' => 'Database/Sqlserver',
-//		'persistent' => false,
-//		'host' => 'iasq4ibfxt.database.windows.net',
-//		'login' => 'knowledges',
-//		'password' => 'temp123$',
-//		'database' => 'knowledges',
-//		'prefix' => '',
-//		//'encoding' => 'utf8',
-//	);
- 
-    
+    public $test = array(
+        'datasource' => 'Database/Mysql',
+        'persistent' => false,
+        'host' => 'localhost',
+        'login' => 'know',
+        'password' => 'temp123$',
+        'database' => 'know',
+        'prefix' => '',
+            //'encoding' => 'utf8',
+    );
     public $default = array(
         'datasource' => 'Database/Mysql',
         'persistent' => false,
